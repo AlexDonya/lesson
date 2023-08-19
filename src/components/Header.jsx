78852499511e11236b24b6c69e045bdddf4f03ac
Alexey <alexeyip68@gmail.com>
2023-08-19 16:57:30 +0300

    Started the course

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <a href="">
+                <img className='header__logo' src="https://www.freepnglogos.com/uploads/youtube-shorts-logo-png/red-shorts-png-logo-with-play-icon-0.png" alt="" />
+            </a>
+        </header>
+    );
+};
+
+export default Header;
