cb6fdca9b5336158059a86d8a2a440c018923fbf
Alexey <alexeyip68@gmail.com>
2023-08-30 17:17:54 +0300

    added NavLink and className = 'active' for links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import s from './Nav.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}><a href='/profile'>Profile</a></div>
-            <div className={`${s.item} ${s.active}`}><a href='/dialogs'>Messages</a></div>
-            <div className={s.item}><a href='/news'>News</a></div>
-            <div className={s.item}><a href='/music'>Music</a></div>
-            <div className={s.item}><a href='/settings'>Settings</a></div>
+            <div className={s.item}>
+                <NavLink to='/profile' className={({ isActive }) =>
+                    isActive ? s.active : ""
+                }>Profile</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to='/dialogs' className={({ isActive }) =>
+                    isActive ? s.active : ""
+                }>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/news' className={({ isActive }) =>
+                    isActive ? s.active : ""
+                }>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/music' className={({ isActive }) =>
+                    isActive ? s.active : ""
+                }>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/settings' className={({ isActive }) =>
+                    isActive ? s.active : ""
+                }>Settings</NavLink>
+            </div>
         </nav>
     );
 };
