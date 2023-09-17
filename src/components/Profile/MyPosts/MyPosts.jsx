ab4af44db561e9ce21a3acd6720125982f560728
Alexey <alexeyip68@gmail.com>
2023-09-17 13:34:43 +0300

    Added Action Creator functions

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
+
 
 const MyPosts = (props) => {
     const postsElements = props.posts.map(p => <Post key={p.id}
@@ -10,12 +12,14 @@ const MyPosts = (props) => {
     const newPostElement = React.createRef();
 
     const addPost = () => {
-        props.dispatch({ type: 'ADD-POST' });
+        props.dispatch(addPostActionCreator());
     };
 
     const onPostChange = () => {
         const text = newPostElement.current.value;
-        props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text, });
+        // let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text, };
+        let action = updateNewPostTextActionCreator(text);
+        props.dispatch(action);
     }
 
     return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -96,6 +96,18 @@ let store = {
     }
 }
 
+export const addPostActionCreator = () => {
+    return {
+        type: 'ADD-POST',
+    }
+}
+
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: 'UPDATE-NEW-POST-TEXT', newText: text,
+    }
+}
+
 window.state = store;
 
 export default store;
