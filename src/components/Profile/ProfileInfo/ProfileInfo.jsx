21e0c5bb89db486621fbc3e0908d82b4851b72c1
Alexey <alexeyip68@gmail.com>
2023-10-29 22:45:52 +0200

    added functionality for opening a photo in the 'ProfileInfo' component

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
+import BigPhoto from './BigPhoto';
 
 const ProfileInfo = (props) => {
+    const [photoActive, setPhotoActive] = useState(false);
+
     if (!props.profile) {
         return <Preloader />;
     }
@@ -10,12 +13,15 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img src="https://img.freepik.com/premium-photo/beach-vacation-aerial-drone-view-tropical-white-sandy-bavaro-beach-punta-cana-dominican-republic-amazing-landscape-with-palms-umbrellas-turquoise-water-atlantic-ocean-wide-format_136401-4046.jpg?w=2000" alt="" />
+                <img src="https://img.freepik.com/premium-photo/beach-vacation-aerial-drone-view-tropical-white-sandy-bavaro-beach-
+                punta-cana-dominican-republic-amazing-landscape-with-palms-umbrellas-turquoise-water-atlantic-ocean-wide-
+                format_136401-4046.jpg?w=2000" alt="" />
             </div>
             <div className={s.info}>
                 <div>{props.profile.fullName}</div>
                 <div className={s.imageProfile}>
-                    <img src={props.profile.photos.large} alt="" />
+                    <img src={props.profile.photos.large} alt="smallPhoto" className={s.photo} onClick={() => setPhotoActive(true)} />
+                    <BigPhoto active={photoActive} setActive={setPhotoActive} bigPhoto={props.profile.photos.large} />
                 </div>
                 <div className={s.descriptionBlock}>{props.profile.aboutMe}</div>
             </div>
